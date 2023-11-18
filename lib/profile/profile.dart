da98437984e9cff711cef994f40db9d570cfd5f2
Siri Tali <siritalilomu@gmail.com>
2023-11-17 18:38:09 -0700

    WIP: merging main into branch

--- lib/profile/profile.dart
+++ lib/profile/profile.dart
@@ -1,13 +1,14 @@
+import 'package:caravanner/theme/colors.dart';
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Jebecky'),
+        title: const Text('People'),
+        backgroundColor: CColors.primary,
       ),
       body: const Padding(
         padding: EdgeInsets.all(16.0),
